cef69fe3d1a8834491ee27afc246025029fac8ce
rlc63 <ibrlouise@Rachel-Carrigs-MacBook-Pro.local>
2016-10-13 18:52:48 -0400

    HW6 draft 1
    draft 1

---
+++ Carrig_Rachel_HW6.R
@@ -0,0 +1,9 @@
+#MAY OR MAY NOT NEED - ggplot2(diamonds,aes(diamonds$carat,diamonds$price))
+
+require(ggplot2)
+data("diamonds")
+
+
+#QUESTION 1 - 
+numdf <- sapply(diamonds, is.numeric)
+hist(numdf)
